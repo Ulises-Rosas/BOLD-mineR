6248a4f8ef4d5f88835faffd2eece269e29d3cbe
Ulises-Rosas <ulisesfrosasp@gmail.com>
2019-07-02 01:45:48 -0500

    adding controls regards validation names and python path

--- r/AuditionBarcodes.R
+++ r/AuditionBarcodes.R
@@ -1,130 +1,328 @@
 library(dplyr)
-
-AuditionBarcodes<- function(species, matches, include_ncbi = F){ ##function for only using with public data
-        frames = lapply(species, function(x){
-
-                meta.by.barcodes1 = SpecimenData(taxon = x) %>%
-                        dplyr::select(processid, bin_uri, species_name, institution_storing) %>%
-                        dplyr::mutate_if(is.factor, as.character) %>%
-                        dplyr::filter(grepl("BOLD", bin_uri),
-                                      !grepl("Mined from GenBank, NCBI", institution_storing),
-                                      !grepl("*unvouchered", institution_storing))
+library(RCurl)
+library(ape)
+library(reticulate)
 
 
-                js0 = getURL(
-                  paste("http://www.boldsystems.org/index.php/API_Tax/TaxonSearch?taxName=",
-                        gsub(" ","%20", x), sep = "")) %>%
-                  gsub('.*\"taxid\":', "", x = .) %>%
-                  gsub(',\"taxon\".*', "", x = .) %>%
-                  paste("http://www.boldsystems.org/index.php/API_Tax/TaxonData?taxId=", . ,
-                        "&dataTypes=all", sep = "") %>% getURL(url = .) %>%
-                  gsub('.*\"depositry\":\\{', "", x = .) %>%
-                  gsub('\\}.*', "", x = .) %>% gsub('\"', "", x = .) %>%
-                  strsplit(x = ., split = ",") %>% .[[1]] %>%
-                  strsplit(x = ., split = "\\:") %>%
-                  lapply(., function(x){
-                    
-                    if(include_ncbi){
-                      
-                      tmp = x[!grepl("*unvouchered", x[1])]
-                      
-                    }else{
-                      
-                      tmp = x[!grepl("Mined from GenBank", x[1]) &
-                                !grepl(" NCBI", x[1]) &
-                                !grepl("*unvouchered", x[1])]
-                    }
-                    
-                    
-                    data.frame(institutions = tmp[1], records = as.numeric(tmp[2]))
-                  }) %>%
-                  do.call("rbind", .) %>%
-                  .[!is.na(.$records),]
 
-                if(nrow(meta.by.barcodes1) == 0 && sum(js0$records, na.rm = T) == 0){
-                        data.frame(Grades = "F",
-                                   Observations = paste("There were ",
-                                                        matches ,
-                                                        " matches. Barcodes mined from GenBank, NCBI.",
-                                                        sep = ""))
-                        }
-                else if(nrow(meta.by.barcodes1) <= 3 && sum(js0$records, na.rm = T) != 0){
-                        data.frame(Grades = "D",
-                                   Observations = paste("There were ",
-                                                        matches ,
-                                                        " matches. Insufficient data. Institution storing: ",
-                                                        length(js0$institutions),
-                                                        ". Specimen records: ",
-                                                        sum(js0$records, na.rm = T),
-                                                        sep = ""))
-                        }
-                else{
-                        if(length(unique(meta.by.barcodes1$bin_uri)) > 1){
-
-                                bin = lapply(unique(meta.by.barcodes1$bin_uri), function(x){
-                                        SpecimenData(bin = x) %>%
-                                                dplyr::select(species_name, institution_storing)  %>%
-                                                dplyr::filter(
-                                                        grepl(" ",species_name),
-                                                        !grepl("Mined from GenBank, NCBI", institution_storing),
-                                                        !grepl("*unvouchered", institution_storing),
-                                                        !grepl(" sp.",species_name)
-                                                ) %>%
-                                                dplyr::group_by(species_name) %>% dplyr::summarise()
-                                })
-
-                                if(length(unique(do.call('rbind', bin)$species_name)) > 1){
-
-
-                                        data.frame(Grades = "E**",
-                                                   Observations = paste("There were ",
-                                                                        matches ,
-                                                                        " matches. Mixtured BIN and it's composed by species such as: ",
-                                                                        paste(unique(do.call('rbind', bin)$species_name), collapse = ", "),
-                                                                        sep = ""))
-                                }
-                                else{
-                                        data.frame(Grades = "C*",
-                                                   Observations = paste("There were ", matches,
-                                                                        " matches. Assessment of intraspecific divergences is still needed.",
-                                                                        sep = ""))
-                                }
-
-                        }
-                        else{
-                                unique.bin = SpecimenData(bin = unique(meta.by.barcodes1$bin_uri)) %>%
-                                        dplyr::select(species_name, institution_storing) %>%
-                                        dplyr::filter(grepl(" ",species_name),
-                                                      !grepl("Mined from GenBank, NCBI", institution_storing),
-                                                      !grepl("*unvouchered", institution_storing),
-                                                      !grepl(" sp.",species_name)) %>%
-                                        dplyr::group_by(species_name, institution_storing) %>%
-                                        dplyr::summarise()
-
-                                if(length(unique(unique.bin$species_name)) == 1 &&
-                                   length(unique(unique.bin$institution_storing)) > 1 ){
-                                        data.frame(Grades = "A",
-                                                   Observations = paste("There were ", matches ,
-                                                                        " matches. External congruence.", sep = ""))
-                                }
-                                else if(length(unique(unique.bin$species_name)) == 1 &&
-                                         length(unique(unique.bin$institution_storing)) == 1){
-                                        data.frame(Grades = "B",
-                                                   Observations = paste("There were ",
-                                                                        matches ,
-                                                                        " matches. Internal congruence.", sep = ""))
-                                }
-                                else{
-                                        data.frame(Grades = "E*",
-                                                   Observations = paste("There were ", matches,
-                                                                        " matches. ", paste(unique(unique.bin$species_name),
-                                                                                            collapse = ","),
-                                                                        " shared the same BIN.",
-                                                                        sep = ""))
-                                }
-                        }
-                }
-
+AuditionBarcodes<- function(species,
+                            matches = NULL,
+                            validate_name = F, 
+                            include_ncbi = F,
+                            python_path = "/usr/local/bin/python3"){ ##function for only using with public data
+  
+  if(validate_name){
+    
+    Sys.setenv(RETICULATE_PYTHON = python_path)
+    
+    use_python(python = python_path)
+    
+    urllib <- reticulate::import("urllib")
+    source_python("python/worms.py", convert = F)
+    
+    getSpps <- function(sppsvec){
+      
+      pat = "^[A-Z][a-z]+ [a-z]+$"
+      pat2 = "^[A-Z][a-z]+ sp[p|\\.]{0,2}$"
+      
+      unlist(
+        lapply(sppsvec, function(x){
+          tmp = as.character(Worms(x)$get_accepted_name())
+  
+          if(!grepl(pat, tmp) || grepl(pat2, tmp))
+            tmp = as.character(Worms(x)$taxamatch())
+          
+          tmp
         })
-        return(do.call('rbind', frames))
+      ) -> tmp2
+      
+      return(tmp2)
+    }
+    
+    renameWithValidate <- function(bin){
+      
+      uniqSpps = unique(bin$species_name)
+      a        = getSpps(uniqSpps)
+      names(a) = uniqSpps
+      
+      sapply(
+        bin$species_name,
+        function(x){
+          a[names(a) == x]
+        }
+      ) -> bin$species_name
+      return(bin)
+    }
+    
+  }
+  
+  json_f <- function(bin){
+    
+    uniqBins = unique(bin$bin_uri)
+    lapply(uniqBins,
+           function(x){
+             df = bin[bin$bin_uri == x,]
+             
+             lapply(
+               unique(df$species_name),
+               function(y){
+                 df2 = df[df$species_name == y, ]
+                 
+                 data.frame(species_name = y,
+                            bin = x, 
+                            institutes = sum(df2$institutes),
+                            n = sum(df2$n),
+                            stringsAsFactors = F)
+               }) -> tmp_list
+             
+             do.call("rbind", tmp_list) -> tmp_df
+             
+             apply(tmp_df,
+                   MARGIN = 1,
+                   function(z){
+                     paste0("'",z[1],"'",':', z[4])
+                   }) -> Spps_N
+             paste0(
+               "'", x, "':{",
+               paste(Spps_N, collapse = ","),
+               "}"
+             )
+           }
+    ) -> tmp_list
+    
+    return(
+      paste(unlist(tmp_list), collapse = ", ")
+    )
+  }
+  
+  classFrame <- function(m,g,b,j,s,y){
+    
+    matchF <- function(ma) paste0("There were ", ma, " matches. ")
+    
+    if(g == "A"){
+      
+      if( is.null(m) ){
+        "Matched BIN with internal congruence only" ->  obs
+        json_f(b) -> bin_str
+        
+      }else{
+        paste0(matchF(m), "External congruence.") -> obs
+      }
+      
+    }else if (g == "B"){
+      
+      if( is.null(m) ){
+        "Matched BIN with internal congruence only" -> obs
+        json_f(b) -> bin_str
+        
+      }else{
+        
+        paste0(matchF(m),"Internal congruence.") -> obs
+      }
+      
+    }else if (g == "C"){
+      
+      if( is.null(m) ){
+        "Splitted BIN" -> obs
+        json_f(b)      -> bin_str
+        
+      }else{
+        paste0(
+          matchF(m),
+          "Assessment of intraspecific divergences is still needed.") -> obs
+      }
+      
+    }else if (g == "D"){
+      
+      paste0(
+        "Insufficient data. Institution storing: ",
+        length(j$institutions),
+        ". Specimen records: "  ,
+        sum(j$records, na.rm = T) ) -> obs
+      
+      NA -> bin_str
+      
+      if( !is.null(m) )
+        paste0( matchF(m), obs ) -> obs
+      
+    }else if (g == "E*"){
+      
+      if( is.null(m) ){
+        "Merged BIN" -> obs
+        json_f(b)    -> bin_str
+        
+      }else{
+        paste0(
+          matchF(m), 
+          paste( s, collapse = ","),
+          " shared the same BIN." ) -> obs
+      }
+      
+    }else if (g == "E**"){
+      
+      if(is.null(m)){
+        "Mixtured BIN" -> obs
+        json_f(b)      -> bin_str
+        
+      }else{
+        paste0(
+          matchF(m),
+          "Mixtured BIN and it's composed by species such as: ",
+          paste(
+            unique(b$species_name),
+            collapse = ", ")) -> obs
+      }
+      
+    }else if (g == "F"){
+
+      "Barcodes mined from GenBank or unvouchered." -> obs
+      NA -> bin_str
+      
+      if( !is.null(m) )
+        paste0( matchF(m), obs) -> obs
+    }
+    
+    df = data.frame(
+      Grades = g,
+      Observations = obs,
+      stringsAsFactors = F  )
+    
+    if(is.null(m))
+      df = cbind(
+        data.frame(Species = y),
+        df,
+        data.frame(BIN_structure = bin_str)
+        )
+    
+    return(df)
+  }
+  
+  frames = lapply(species, function(x){
+    
+    meta.by.barcodes0 = SpecimenData(taxon = x) %>%
+      dplyr::select(processid,
+                    bin_uri,
+                    species_name,
+                    institution_storing,
+                    species_taxID)
+    
+    taxid = unique(meta.by.barcodes0$species_taxID)[1]
+    
+    meta.by.barcodes1 = meta.by.barcodes0%>%
+      dplyr::mutate_if(is.factor, as.character) %>%
+      dplyr::filter(grepl("BOLD", bin_uri),
+                    !grepl("*unvouchered", institution_storing))
+    
+    if(include_ncbi){
+      meta.by.barcodes1 = meta.by.barcodes1 %>% 
+        dplyr::filter(!grepl("Mined from GenBank, NCBI", institution_storing))
+    }
+      
+    if( nrow(meta.by.barcodes1) <= 3){
+      
+      gsub('\"',"",
+        gsub(
+          '.*depositry":\\{(.+?)\\},.*', '\\1', 
+          getURL(
+            paste0(
+              "http://www.boldsystems.org/index.php/API_Tax/TaxonData?taxId=",
+              taxid ,"&dataTypes=all" )))) -> js00 
+      
+      do.call("rbind",
+              lapply(
+                strsplit(
+                  strsplit( js00, split = ",")[[1]], split = "\\:"),
+                
+                function(x){
+                  
+                  if(include_ncbi){
+                    tmp = x[!grepl("*unvouchered", x[1])]
+                    
+                  }else{
+                    tmp = x[!grepl("Mined from GenBank", x[1]) &
+                              !grepl(" NCBI", x[1]) &
+                              !grepl("*unvouchered", x[1])] }
+                  
+                  if( !is.na(tmp[2]) )
+                    data.frame(institutions = tmp[1],
+                               records = as.numeric(tmp[2]), 
+                               stringsAsFactors = F)
+                })
+              ) -> js0
+      
+      if( sum(js0$records, na.rm = T) > 0){
+        
+        classFrame(m = matches, g = "D", j = js0, y = x)
+      }else{
+        
+        classFrame(m = matches, g = "F", y = x)
+      }
+      
+    }else{
+      
+      uniqBins = unique(meta.by.barcodes1$bin_uri)
+      bin = SpecimenData(bin = paste(uniqBins, collapse = "|")) %>%
+        dplyr::filter(grepl("[A-Z][a-z]+ [a-z]+$",species_name),
+                      !grepl("[A-Z][a-z]+ sp[p|.]{0,2}$", species_name),
+                      !grepl("*unvouchered", institution_storing))
+      
+      if(include_ncbi){
+        bin = bin %>%
+          dplyr::filter(!grepl("Mined from GenBank, NCBI", institution_storing))
+      }
+      
+      uniqSpps  = unique(bin$species_name)
+      lengthIns = length(unique(bin$institution_storing))
+      
+      bin = bin %>%
+        dplyr::group_by( species_name, bin_uri) %>%
+        dplyr::summarise(institutes = length(unique(institution_storing)),
+                                  n = length(species_name))
+      
+      if( length(uniqBins) > 1 ){
+        
+        if( length(uniqSpps) > 1 ){
+          
+          if(validate_name)
+            bin = renameWithValidate(bin)
+
+          classFrame(
+            m = matches,
+            g = ifelse(length(unique(bin$species_name)) == 1, 'C', 'E**'),
+            b = bin,
+            y = x)
+          
+        }else{
+            classFrame(m = matches, g = 'C', b = bin, y = x) 
+        }
+        
+      }else{
+      
+        if( length(uniqSpps) == 1 && lengthIns > 1){
+          classFrame(m = matches, g= 'A', b = bin, y = x ) 
+          
+        }else if( length(uniqSpps) == 1 && lengthIns == 1 ){
+          classFrame(m = matches, g= 'B', b = bin, y = x ) 
+          
+        }else{
+          
+          if(validate_name)
+            bin = renameWithValidate(bin)
+          
+          if( length(unique(bin$species_name)) == 1){
+            
+            classFrame(
+              m = matches,
+              g = ifelse(lengthIns > 1, 'A', 'B'),
+              b = bin, 
+              y = x )
+            
+          }else{
+            classFrame(m = matches, g = 'E*', b = bin, s = uniqSpps, y =  x)
+          }
+        }
+      }
+    }
+  })
+  return(do.call('rbind', frames))
 }
